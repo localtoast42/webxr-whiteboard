cea8bde59e24fe24a81821c6764afd1a337ac958
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-10-11 13:43:19 -0400

    Remove EffectComposer

--- src/engine/renderEngine.ts
+++ src/engine/renderEngine.ts
@@ -1,12 +1,9 @@
 import * as THREE from "three";
-import { EffectComposer } from "three/addons/postprocessing/EffectComposer.js";
-import { RenderPass } from "three/addons/postprocessing/RenderPass.js";
 import { Engine } from "./engine";
 import { Entity } from "./entity";
 
 export class RenderEngine implements Entity {
   public renderer: THREE.WebGLRenderer;
-  composer: EffectComposer;
 
   constructor(private engine: Engine) {
     this.renderer = new THREE.WebGLRenderer({
@@ -18,23 +15,14 @@ export class RenderEngine implements Entity {
 
     this.renderer.setSize(this.engine.sizes.width, this.engine.sizes.height);
     this.renderer.setPixelRatio(Math.min(this.engine.sizes.pixelRatio, 2));
-
-    this.composer = new EffectComposer(this.renderer);
-
-    const renderPass = new RenderPass(
-      this.engine.scene,
-      this.engine.camera.instance
-    );
-    this.composer.addPass(renderPass);
   }
 
   update() {
-    this.composer.render();
+    this.renderer.render(this.engine.scene, this.engine.camera.instance);
   }
 
   resize() {
     this.renderer.setSize(this.engine.sizes.width, this.engine.sizes.height);
-    this.composer.setSize(this.engine.sizes.width, this.engine.sizes.height);
-    this.composer.render();
+    this.update();
   }
 }
